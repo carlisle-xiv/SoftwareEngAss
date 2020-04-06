51d44952b256d570d996daddc48e7b35185fd8f4
aibenStunner <ebenezergadri99@gmail.com>
2020-04-06 23:42:44 +0000

    Update form_card.dart

--- lib/components/form_card.dart
+++ lib/components/form_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FormCard extends StatelessWidget {
   final bool showForgot;
@@ -9,69 +8,72 @@ class FormCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    TextTheme textTheme = Theme.of(context).textTheme;
+
     return new Container(
-      width: double.infinity,
-      height: ScreenUtil.getInstance().setHeight(540),
+      width: screenWidth,
+      height: screenHeight * 0.405,
       decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(8.0),
+          borderRadius: BorderRadius.circular(screenHeight * 0.015),
           boxShadow: [
             BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0.0, 15.0),
-                blurRadius: 15.0),
+              color: Colors.black12,
+              offset: Offset(0.0, screenHeight * 0.02),
+              blurRadius: screenHeight * 0.02,
+            ),
             BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0.0, -10.0),
-                blurRadius: 10.0),
+              color: Colors.black12,
+              offset: Offset(0.0, screenHeight * -0.015),
+              blurRadius: screenHeight * 0.015,
+            ),
           ]),
       child: Padding(
-        padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+        padding: EdgeInsets.only(
+          left: screenWidth * 0.038,
+          right: screenWidth * 0.038,
+          top: screenHeight * 0.023,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(text,
-                style: TextStyle(
-                    fontSize: ScreenUtil.getInstance().setSp(45),
+                style: textTheme.headline.copyWith(
                     letterSpacing: .6,
-                    fontWeight: FontWeight.w800)),
+                    fontWeight: FontWeight.w800,
+                    fontSize: 24.8)),
             SizedBox(
-              height: ScreenUtil.getInstance().setHeight(30),
+              height: screenHeight * 0.0225,
             ),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Email',
-                labelStyle: TextStyle(
-                  fontSize: ScreenUtil.getInstance().setSp(35),
-                ),
+                labelStyle: TextStyle(fontSize: 19.3),
               ),
               onChanged: (value) {},
             ),
             SizedBox(
-              height: ScreenUtil.getInstance().setHeight(30),
+              height: screenHeight * 0.0225,
             ),
             TextField(
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Password',
-                labelStyle: TextStyle(
-                  fontSize: ScreenUtil.getInstance().setSp(35),
-                ),
+                labelStyle: TextStyle(fontSize: 19.3),
               ),
               onChanged: (value) {},
             ),
             Padding(
-              padding:
-                  EdgeInsets.only(top: ScreenUtil.getInstance().setHeight(60)),
+              padding: EdgeInsets.only(top: screenHeight * 0.045),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   Text(
                     showForgot ? "Forgot Password?" : "",
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: ScreenUtil.getInstance().setSp(25),
-                        fontWeight: FontWeight.w600),
+                    style: textTheme.body1.copyWith(
+                        color: Colors.blue, fontWeight: FontWeight.w800),
                   )
                 ],
               ),
