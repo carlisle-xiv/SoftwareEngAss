78d15e6432876581e9282120aaa77ae8e13c40cb
aibenStunner <ebenezergadri99@gmail.com>
2020-04-06 23:40:34 +0000

    main.dart commit

---
+++ lib/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:gods_eye/models/stream_model/stream_data.dart';
+import 'package:gods_eye/models/sub_stream_model/camera_streams.dart';
+import 'package:gods_eye/screens/login_screen.dart';
+import 'package:gods_eye/screens/sign_up_screen.dart';
+import 'package:gods_eye/screens/stream_screen/stream_fullscreen.dart';
+import 'package:provider/provider.dart';
+import 'screens/main_nav.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => StreamData()),
+        ChangeNotifierProvider(create: (_) => CameraStreams()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: LoginScreen.id,
+        routes: {
+          LoginScreen.id: (context) => LoginScreen(),
+          SignUpScreen.id: (context) => SignUpScreen(),
+          MainNav.id: (context) => MainNav(),
+          StreamFullScreen.id: (context) => StreamFullScreen(),
+        },
+        theme: ThemeData(
+          fontFamily: 'Gilroy',
+        ),
+      ),
+    );
+  }
+}
