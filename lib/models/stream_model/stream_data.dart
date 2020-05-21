5576efc8b434f4d31a04ca1bb593c3e6ed135a2c
aibenStunner <ebenezergadri99@gmail.com>
2020-05-21 13:02:47 +0000

    Update stream_data.dart

--- lib/models/stream_model/stream_data.dart
+++ lib/models/stream_model/stream_data.dart
@@ -7,7 +7,13 @@ class StreamData extends ChangeNotifier {
 
   List<Stream> _streams = [
     Stream(title: "Cafeteria - East", imgSrc: "images/streams/cafe_east.jpg"),
-    Stream(title: "Penguin Room", imgSrc: "images/streams/penguin_room.jpg"),
+    Stream(title: "Cafeteria - West", imgSrc: "images/streams/cafe_west.jpg"),
+    Stream(
+        title: "Penguin Room - North",
+        imgSrc: "images/streams/penguin_room_north.jpg"),
+    Stream(
+        title: "Penguin Room - South",
+        imgSrc: "images/streams/penguin_room.jpg"),
     Stream(
         title: "Playground - East",
         imgSrc: "images/streams/playground_east.jpg"),
@@ -37,6 +43,10 @@ class StreamData extends ChangeNotifier {
 
   int get currentStream {
     return _selectedStream;
+  }
+
+  String get currentStreamTitle {
+    return streams[currentStream].title;
   }
 
   void updateCurrentStream(int index) {
