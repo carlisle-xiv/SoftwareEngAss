7a281552cffb6e080ae7cde5f3263968f8188e49
aibenStunner <ebenezergadri99@gmail.com>
2020-05-22 13:47:00 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gods_eye/models/children_models/children_data.dart';
 import 'package:gods_eye/models/stream_model/stream_data.dart';
+import 'package:gods_eye/models/teacher_model/teachers_data.dart';
 import 'package:gods_eye/models/sub_stream_model/camera_streams.dart';
 import 'package:gods_eye/screens/login_screen/login_screen.dart';
 import 'package:gods_eye/screens/sign_up_screen/sign_up_screen.dart';
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => StreamData()),
         ChangeNotifierProvider(create: (_) => CameraStreams()),
         ChangeNotifierProvider(create: (_) => ChildrenData()),
+        ChangeNotifierProvider(create: (_) => TeachersData()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
