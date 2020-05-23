2977fe514aef21a0cb19ca107671632265ddc54f
sese <aakorlikwasi@gmail.com>
2020-05-22 13:38:44 -1200

    added school screen dependency model

---
+++ lib/models/teacher_model/teacher.dart
@@ -0,0 +1,5 @@
+class Teacher{
+  final String name, contact, imgSrc;
+
+  Teacher({this.name, this.contact, this.imgSrc});
+}
---
+++ lib/models/teacher_model/teacher_data.dart
@@ -0,0 +1,53 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
+import 'package:gods_eye/models/teacher_model/teacher.dart';
+
+class TeachersData extends ChangeNotifier {
+  List<Teacher> _teachers = [
+    Teacher(
+        name: "Mr. Okonkuta",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+    Teacher(
+        name: "Mr. Okonkwo",
+        contact: "024 123 4567",
+        imgSrc: "images/father.jpg"),
+  ];
+
+  UnmodifiableListView<Teacher> get teachers {
+    return UnmodifiableListView(_teachers);
+  }
+
+  int get teachersCount {
+    return _teachers.length;
+  }
+}
